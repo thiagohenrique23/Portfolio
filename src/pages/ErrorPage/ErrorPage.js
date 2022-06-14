18d3991bb7068cc857fca2c8341b51038cf533ec
thiagohenrique23 <thiagohenrique2309@gmail.com>
2022-06-14 08:44:59 -0300

    Consertando erros

--- src/pages/ErrorPage/ErrorPage.js
+++ src/pages/ErrorPage/ErrorPage.js
@@ -1,10 +1,20 @@
+import { Button } from "@mui/material";
 import React from "react";
+import { ErrorImage, ErrorPageContainer, TitleCard } from "./styled";
+import error from "../../assets/error.png"
+import { useNavigate } from "react-router-dom";
+import { goToMain } from "../../routes/coordinator";
 
 const ErrorPage = () => {
+
+  const Navigate = useNavigate()
+
   return (
-    <div>
-      ErrorPage
-    </div>
+  <ErrorPageContainer>
+    <TitleCard>Ops parece que aconteceu um erro na página</TitleCard>
+    <ErrorImage src={error}/> 
+    <Button onClick={() => goToMain(Navigate)}>Volte para página principal</Button>
+  </ErrorPageContainer>
   )
 }
 
